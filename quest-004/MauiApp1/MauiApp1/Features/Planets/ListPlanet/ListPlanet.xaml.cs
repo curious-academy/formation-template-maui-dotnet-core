99e0fe78f0c0cd38f3b4fafbfb18a2d7dbad7c85
Evan BOISSONNOT <evan@boissonnot.fr>
2023-08-29 12:29:24 +0200

    fix: update collection

---
+++ quest-004/MauiApp1/MauiApp1/Features/Planets/ListPlanet/ListPlanet.xaml.cs
@@ -0,0 +1,50 @@
+using MauiApp1.Features.Planets.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiApp1.Features.Planets.ListPlanet;
+
+public partial class ListPlanet : ContentPage
+{
+    #region Fields
+    private Features.Planets.Models.Planets planets = new Features.Planets.Models.Planets();
+    #endregion
+
+    public ListPlanet()
+    {
+        InitializeComponent();
+
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        this.DisplayPlanets();
+    }
+
+    private async Task btnGoBack_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private void DisplayPlanets()
+    {
+        this.lstPlanets.ItemsSource = new ObservableCollection<Planet>(Planets.Models.Planets.List);
+
+        //DisplayActionSheet("Hello", "Cancel ?", "Destroy", FlowDirection.LeftToRight);
+        //DisplayAlert("Hello !", "Yeah", "Cancel", FlowDirection.LeftToRight);
+    }
+
+    private async void lstPlanets_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (this.lstPlanets.SelectedItem != null)
+        {
+            var planet = this.lstPlanets.SelectedItem as Planet;
+            await Shell.Current.GoToAsync($"{nameof(AddPlanet.AddPlanet)}?Id={planet.Id}");
+        }
+    }
+
+    private void lstPlanets_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        this.lstPlanets.SelectedItem = null;
+    }
+}
---
+++ quest-004/MauiApp1/MauiApp1/Features/Planets/Models/Planets.cs
@@ -0,0 +1,23 @@
+﻿namespace MauiApp1.Features.Planets.Models
+{
+    public class Planets
+    {
+        #region Fields
+        #endregion
+
+        #region Public methods
+        public Planet GetById(int id)
+        {
+            return List.First(p => p.Id == id);
+        }
+        #endregion
+
+        #region Properties
+        public static List<Planet> List { get; } = new()
+        {
+            new (1, "Coruscant", "bl"),
+            new (2, "Kashhyk", "bla")
+        };
+        #endregion
+    }
+}
