02a6f2c7725837ff81c9c96f6f37a2b3ab945c1b
Evan BOISSONNOT <evan@boissonnot.fr>
2023-08-29 18:00:09 +0200

    feat: add delete from menu item

---
+++ quest-005/MauiApp1/MauiApp1/Features/Planets/Models/Planets.cs
@@ -0,0 +1,28 @@
+﻿namespace MauiApp1.Features.Planets.Models
+{
+    public class Planets
+    {
+        #region Fields
+        #endregion
+
+        #region Public methods
+        public Planet GetById(int id)
+        {
+            return List.First(p => p.Id == id);
+        }
+
+        public void Delete(Planet item)
+        {
+            List.Remove(item);
+        }
+        #endregion
+
+        #region Properties
+        public static List<Planet> List { get; } = new()
+        {
+            new (1, "Coruscant", "bl"),
+            new (2, "Kashhyk", "bla")
+        };
+        #endregion
+    }
+}
