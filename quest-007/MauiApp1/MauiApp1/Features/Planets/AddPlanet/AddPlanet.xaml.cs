ab6a60807cede752c29570e156d6c66b7b258a1e
Evan BOISSONNOT <evan@boissonnot.fr>
2023-08-29 19:03:35 +0200

    feat: add class library

---
+++ quest-007/MauiApp1/MauiApp1/Features/Planets/AddPlanet/AddPlanet.xaml.cs
@@ -0,0 +1,39 @@
+using AllModels.Planets;
+
+namespace MauiApp1.Features.Planets.AddPlanet;
+
+[QueryProperty(nameof(PlanetId), "Id")]
+public partial class AddPlanet : ContentPage
+{
+    #region Fields
+    private AllModels.Planets.Planets planets = new AllModels.Planets.Planets();
+    #endregion
+
+    #region Constructors
+    public AddPlanet()
+    {
+        InitializeComponent();
+    }
+    #endregion
+
+    #region Internal methods
+    private void btnSave_Clicked(object sender, EventArgs e)
+    {
+        this.CurrentItem.Set(this.txtName.Text);
+        Shell.Current.GoToAsync("..");
+    }
+    #endregion
+
+    #region Properties
+    public int PlanetId
+    {
+        set
+        {
+            this.CurrentItem = this.planets.GetById(value);
+            this.txtName.Text = this.CurrentItem.Name;
+        }
+    }
+
+    public Planet CurrentItem { get; set; }
+    #endregion
+}
---
+++ quest-007/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MauiApp1.Features.Planets.AddPlanet;
+using MauiApp1.Features.Planets.ListPlanet;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        // SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+
+    private void btnGoToListPlanets_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(ListPlanet));
+    }
+
+    private void btnGoToAddPlanet_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddPlanet));
+    }
+}
+
