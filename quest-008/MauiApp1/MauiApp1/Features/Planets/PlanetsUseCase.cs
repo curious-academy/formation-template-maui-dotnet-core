901ba13cbcd43c54e82d0162f598d0a3a012dd47
Evan BOISSONNOT <evan@boissonnot.fr>
2023-08-30 18:32:44 +0200

    feat: end of quest

---
+++ quest-008/MauiApp1/MauiApp1/Features/Planets/PlanetsUseCase.cs
@@ -0,0 +1,28 @@
+﻿using AllModels.Planets;
+using Interfaces;
+
+namespace MauiApp1.Features.Planets
+{
+    public class PlanetsUseCase
+    {
+        #region Constructors
+        public PlanetsUseCase(IGetPlanets getPlanets)
+        {
+            this.GetPlanets = getPlanets;
+        }
+        #endregion
+
+        #region Public methods
+        public async Task<List<Planet>> GetAllAsync(string filter = "")
+        {
+            var result = await this.GetPlanets.GetAll();
+
+            return result.ToList();
+        }
+        #endregion
+
+        #region Properties
+        public IGetPlanets GetPlanets { get; init; }
+        #endregion
+    }
+}
