5437e88de19bc5fa5cfd3aec47c65e379f9101f0
Evan BOISSONNOT <evan@boissonnot.fr>
2023-09-06 17:51:16 +0200

    feat: lifecycles attached

--- quest-011/MauiApp1/MauiApp1/App.xaml.cs
+++ quest-011/MauiApp1/MauiApp1/App.xaml.cs
@@ -2,10 +2,30 @@
 
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    public App()
+    {
+        InitializeComponent();
 
-		MainPage = new AppShell();
-	}
+        MainPage = new AppShell();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) => { Application.Current.MainPage.DisplayAlert("Created", "Created", "ok", "no"); }; // Après création fenêtre native, fenetre peut etre pas encore visible
+
+        window.Activated += (s, e) => { Application.Current.MainPage.DisplayAlert("Activated", "Activated", "ok", "no"); }; // fenetre active / prioritaire
+
+        window.Deactivated += (s, e) => { Application.Current.MainPage.DisplayAlert("Deactivated", "Deactivated", "ok", "no"); }; // plus prio, mais peut etre encore visible
+
+        window.Stopped += (s, e) => { Application.Current.MainPage.DisplayAlert("Stopped", "Stopped", "ok", "no"); }; // plus visible
+
+        window.Resumed += (s, e) => { Application.Current.MainPage.DisplayAlert("Resumed", "Resumed", "ok", "no"); };  // application reprend, après stopped, obligatoirement
+
+        window.Destroying += (s, e) => { Application.Current.MainPage.DisplayAlert("Destroying", "Destroying", "ok", "no"); }; // fenetre native détruite / libérée
+
+        return window;
+    }
 }
+
--- quest-011/MauiApp1/MauiApp1/MauiProgram.cs
+++ quest-011/MauiApp1/MauiApp1/MauiProgram.cs
@@ -5,6 +5,7 @@ using MauiApp1.Features.Planets.AddPlanet.ViewModels;
 using MauiApp1.Features.Planets.ListPlanet;
 using MauiApp1.Features.Planets.ListPlanet.ViewModels;
 using MauiApp1.Shared.UIs.Dialogs;
+using Microsoft.Maui.LifecycleEvents;
 using Services;
 using SQLite.Plugins;
 
@@ -17,6 +18,31 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .ConfigureLifecycleEvents(events =>
+            {
+#if ANDROID
+                events.AddAndroid(android => android
+                    .OnActivityResult((activity, requestCode, resultCode, data) => LogEvent(nameof(AndroidLifecycle.OnActivityResult), requestCode.ToString()))
+                    .OnStart((activity) => LogEvent(nameof(AndroidLifecycle.OnStart)))
+                    .OnCreate((activity, bundle) => LogEvent(nameof(AndroidLifecycle.OnCreate)))
+                    .OnBackPressed((activity) => LogEvent(nameof(AndroidLifecycle.OnBackPressed)) && false)
+                    .OnStop((activity) => LogEvent(nameof(AndroidLifecycle.OnStop))));
+#endif
+
+#if IOS
+                events.AddiOS(ios => ios
+                        .OnActivated((app) => LogEvent(nameof(iOSLifecycle.OnActivated)))
+                        .OnResignActivation((app) => LogEvent(nameof(iOSLifecycle.OnResignActivation)))
+                        .DidEnterBackground((app) => LogEvent(nameof(iOSLifecycle.DidEnterBackground)))
+                        .WillTerminate((app) => LogEvent(nameof(iOSLifecycle.WillTerminate))));
+#endif
+
+                static bool LogEvent(string eventName, string type = null)
+                {
+                    System.Diagnostics.Debug.WriteLine($"Lifecycle event: {eventName}{(type == null ? string.Empty : $" ({type})")}");
+                    return true;
+                }
+            })
             .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
