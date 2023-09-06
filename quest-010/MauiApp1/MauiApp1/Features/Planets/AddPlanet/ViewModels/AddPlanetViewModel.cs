110bed5f1431510ac73535a5b7c5794403b26b0d
Evan BOISSONNOT <evan@boissonnot.fr>
2023-09-06 17:09:35 +0200

    feat: add info box

--- quest-010/MauiApp1/MauiApp1/Features/Planets/AddPlanet/ViewModels/AddPlanetViewModel.cs
+++ quest-010/MauiApp1/MauiApp1/Features/Planets/AddPlanet/ViewModels/AddPlanetViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Interfaces;
+using MauiApp1.Shared.UIs.Dialogs;
 
 namespace MauiApp1.Features.Planets.AddPlanet.ViewModels
 {
@@ -9,7 +10,7 @@ namespace MauiApp1.Features.Planets.AddPlanet.ViewModels
     {
         #region Fields
         private readonly IAddOnePlanet addOnePlanet;
-
+        private readonly IDialogService dialogService;
         private Planet planet = new Planet(0, "", "");
 
         [ObservableProperty]
@@ -17,9 +18,10 @@ namespace MauiApp1.Features.Planets.AddPlanet.ViewModels
         #endregion
 
         #region Constructors
-        public AddPlanetViewModel(IAddOnePlanet addOnePlanet)
+        public AddPlanetViewModel(IAddOnePlanet addOnePlanet, IDialogService dialogService)
         {
             this.addOnePlanet = addOnePlanet;
+            this.dialogService = dialogService;
         }
         #endregion
 
@@ -27,7 +29,7 @@ namespace MauiApp1.Features.Planets.AddPlanet.ViewModels
         [RelayCommand]
         public async Task SaveOne()
         {
-
+            await this.dialogService.ShowConfirmationAsync("Save is done", "Data are saved");
         }
         #endregion
 
